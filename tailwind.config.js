524646b141d0ca184097c3fc5d9c54758e9d2437
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2022-05-02 20:10:33 +0100

    font tailtwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     fontFamily: {
-      custom1: ["Inter-Medium", "sans-serif"],
-      custom2: ["Inter-Regular", "sans-serif"],
+      interMedium: ["Inter-Medium", "sans-serif"],
+      interRegular: ["Inter-Regular", "sans-serif"],
     },
     extend: {},
   },
